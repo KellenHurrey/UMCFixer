7e804c10aa591f8d01c72ff73b0161c171fca7fb
KellenHurrey <kellenhurrey@gmail.com>
2024-06-27 13:22:24 -0700

    new nullpointer

--- src/main/java/com/kellen/umcfixer/mixins/immersiverailroading/ImmersiveRailroadingMixin.java
+++ src/main/java/com/kellen/umcfixer/mixins/immersiverailroading/ImmersiveRailroadingMixin.java
@@ -5,12 +5,14 @@ import cam72cam.immersiverailroading.entity.EntityRollingStock;
 import cam72cam.mod.MinecraftClient;
 import cam72cam.mod.entity.Entity;
 import cam72cam.mod.entity.Player;
+import cam72cam.mod.event.ClientEvents;
 import cam72cam.mod.render.opengl.RenderState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(value = ImmersiveRailroading.class, remap = false)
 abstract class ImmersiveRailroadingMixin{
@@ -30,6 +32,21 @@ abstract class ImmersiveRailroadingMixin{
             }
         }
         ci.cancel();
+    }
+
+    @Inject(method = "lambda$clientEvent$4(Lcam72cam/mod/event/ClientEvents$MouseGuiEvent;)Ljava/lang/Boolean;", at = @At("HEAD"), cancellable = true)
+    private static void lambda$clientEvent$4(ClientEvents.MouseGuiEvent evt, CallbackInfoReturnable<Boolean> cir){
+        if (!MinecraftClient.isReady() || MinecraftClient.getPlayer() == null) {
+            cir.setReturnValue(true);
+        } else {
+            Entity riding = MinecraftClient.getPlayer().getRiding();
+            if (!(riding instanceof EntityRollingStock)) {
+                cir.setReturnValue(true);
+            } else {
+                EntityRollingStock stock = (EntityRollingStock)riding;
+                cir.setReturnValue(stock.getDefinition().getOverlay() == null || stock.getDefinition().getOverlay().click(evt, stock));
+            }
+        }
     }
 
 }
