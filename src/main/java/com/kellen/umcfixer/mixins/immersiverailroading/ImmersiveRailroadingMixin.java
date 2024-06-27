99232d7a80cbc7818a573503890c44c08457f4cb
KellenHurrey <kellenhurrey@gmail.com>
2024-06-27 12:04:57 -0700

    ahh

--- src/main/java/com/kellen/umcfixer/mixins/immersiverailroading/ImmersiveRailroadingMixin.java
+++ src/main/java/com/kellen/umcfixer/mixins/immersiverailroading/ImmersiveRailroadingMixin.java
@@ -1,7 +1,10 @@
 package com.kellen.umcfixer.mixins.immersiverailroading;
 
 import cam72cam.immersiverailroading.ImmersiveRailroading;
+import cam72cam.immersiverailroading.entity.EntityRollingStock;
 import cam72cam.mod.MinecraftClient;
+import cam72cam.mod.entity.Entity;
+import cam72cam.mod.entity.Player;
 import cam72cam.mod.render.opengl.RenderState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
@@ -14,9 +17,19 @@ abstract class ImmersiveRailroadingMixin{
 
     @Inject(method = "lambda$clientEvent$3(Lcam72cam/mod/render/opengl/RenderState;F)V", at = @At("HEAD"), cancellable = true)
     private static void lambda$clientEvent$3(RenderState state, float pt, CallbackInfo ci){
-        if (!MinecraftClient.isReady() || MinecraftClient.getPlayer() == null){
+        Player player = MinecraftClient.getPlayer();
+        if (player == null) {
             ci.cancel();
+            return;
         }
+        Entity riding = player.getRiding();
+        if (riding instanceof EntityRollingStock) {
+            EntityRollingStock stock = (EntityRollingStock)riding;
+            if (stock.getDefinition().getOverlay() != null) {
+                stock.getDefinition().getOverlay().render(state, stock);
+            }
+        }
+        ci.cancel();
     }
 
 }
